b3f6e24043131f53071a73fb7c784fddf58387f5
Cristhian Valencia <cvalencia@clarika.com.ar>
2018-04-29 19:58:03 -0300

    model binding

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,12 @@ namespace DutchTreat.Controllers
         public IActionResult Contact()
         {
             ViewBag.Title = "Contact us!";
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
             return View();
         }
 
---
+++ DutchTreat/ViewModels/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Nombre { get; set; }
+        public string Mensaje { get; set; }
+        public string Asunto { get; set; }
+        public string Email { get; set; }
+    }
+}
