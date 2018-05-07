71b71ee1f2c77832e0113f6d2903b5f9a47e156f
Cristhian Valencia <valenciacristhian47@gmail.com>
2018-05-07 06:53:18 -0300

    json serializer and get order by id

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,14 @@ namespace DutchTreat.Data
             _logger = logger;
         }
 
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _ctx.Orders
+                .Include(o => o.Items)
+                .ThenInclude(i => i.Product)
+                .ToList();
+        }
+
         public IEnumerable<Product> GetAllProducts()
         {
             try
@@ -34,6 +43,15 @@ namespace DutchTreat.Data
                 throw;
             }
             
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return _ctx.Orders
+               .Include(o => o.Items)
+               .ThenInclude(i => i.Product)
+               .Where(o => o.Id == id)
+               .FirstOrDefault();
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
